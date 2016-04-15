bb4e84da1a996f216e13450dc2f22cb380f78678
gleung93 <gleung93@gmail.com>
2016-04-15 01:52:47 -0400

    added twitter page

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -38,6 +38,20 @@ define(["jquery", "mixitup", "drag", "magnific-popup"], function($) {
         }
     });
 
+    /*Smooth Scroll to Projects*/
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top -75
+          }, 1000, 'swing');
+          return false;
+        }
+      }
+    });
+
     //Mix it Up
     var filterState;
     var graphicState = true;
